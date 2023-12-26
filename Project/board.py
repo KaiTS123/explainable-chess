1a6ae66101b84e3f8496837332446204b3f1ae04
kaitannashah <kaitannashah@gmail.com>
2023-12-26 00:57:43 +0000

    piece positioning added to evaluation

--- Project/board.py
+++ Project/board.py
@@ -31,6 +31,66 @@ class TTEntry():
 
 
 class Board:
+    PAWN_POS_TABLE = [0,   0,   0,   0,   0,   0,  0,   0,
+                      -35,  -1, -20, -23, -15,  24, 38, -22,
+                      -26,  -4,  -4, -10,   3,   3, 33, -12,
+                      -27,  -2,  -5,  12,  17,   6, 10, -25,
+                      -14,  13,   6,  21,  23,  12, 17, -23,
+                      -6,   7,  26,  31,  65,  56, 25, -20,
+                      98, 134,  61,  95,  68, 126, 34, -11,
+                      0,   0,   0,   0,   0,   0,  0,   0
+                      ]
+
+    KNIGHT_POS_TABLE = [-105, -21, -58, -33, -17, -28, -19,  -23,
+                        -29, -53, -12,  -3,  -1,  18, -14,  -19,
+                        -23,  -9,  12,  10,  19,  17,  25,  -16,
+                        -13,   4,  16,  13,  28,  19,  21,   -8,
+                        -9,  17,  19,  53,  37,  69,  18,   22,
+                        -47,  60,  37,  65,  84, 129,  73,   44,
+                        -73, -41,  72,  36,  23,  62,   7,  -17,
+                        -167, -89, -34, -49,  61, -97, -15, -107
+                        ]
+
+    BISHOP_POS_TABLE = [-33,  -3, -14, -21, -13, -12, -39, -21,
+                        4,  15,  16,   0,   7,  21,  33,   1,
+                        0,  15,  15,  15,  14,  27,  18,  10,
+                        -6,  13,  13,  26,  34,  12,  10,   4,
+                        -4,   5,  19,  50,  37,  37,   7,  -2,
+                        -16,  37,  43,  40,  35,  50,  37,  -2,
+                        -26,  16, -18, -13,  30,  59,  18, -47,
+                        -29,   4, -82, -37, -25, -42,   7,  -8,
+                        ]
+
+    ROOK_POS_TABLE = [-19, -13,   1,  17, 16,  7, -37, -26,
+                      -44, -16, -20,  -9, -1, 11,  -6, -71,
+                      -45, -25, -16, -17,  3,  0,  -5, -33,
+                      -36, -26, -12,  -1,  9, -7,   6, -23,
+                      -24, -11,   7,  26, 24, 35,  -8, -20,
+                      -5,  19,  26,  36, 17, 45,  61,  16,
+                      27,  32,  58,  62, 80, 67,  26,  44,
+                      32,  42,  32,  51, 63,  9,  31,  43,
+                      ]
+
+    QUEEN_POS_TABLE = [-1, -18,  -9,  10, -15, -25, -31, -50,
+                       -35,  -8,  11,   2,   8,  15,  -3,   1,
+                       -14,   2, -11,  -2,  -5,   2,  14,   5,
+                       -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+                       -27, -27, -16, -16,  -1,  17,  -2,   1,
+                       -13, -17,   7,   8,  29,  56,  47,  57,
+                       -24, -39,  -5,   1, -16,  57,  28,  54,
+                       -28,   0,  29,  12,  59,  44,  43,  45,
+                       ]
+
+    KING_POS_TABLE = [-15,  36,  12, -54,   8, -28,  24,  14,
+                      1,   7,  -8, -64, -43, -16,   9,   8,
+                      -14, -14, -22, -46, -44, -30, -15, -27,
+                      -49,  -1, -27, -39, -46, -44, -33, -51,
+                      -17, -20, -12, -27, -30, -25, -14, -36,
+                      -9,  24,   2, -16, -20,   6,  22, -22,
+                      29,  -1, -20,  -7,  -8,  -4, -38, -29,
+                      -65,  23,  16, -15, -56, -34,   2,  13,
+                      ]
+
     def __init__(self, FEN=None, orig=None) -> None:
         if not orig and not FEN:
             FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
@@ -1045,14 +1105,46 @@ class Board:
         self.pieces = self.blackPieces | self.whitePieces
 
     def evalMaterial(self) -> int:
-        whiteMaterial = self.whitePawns.count() + self.whiteBishops.count()*3 + self.whiteKnights.count()*3 + self.whiteRooks.count()*5 + self.whiteQueens.count()*9
-        blackMaterial = self.blackPawns.count() + self.blackBishops.count()*3 + self.blackKnights.count()*3 + self.blackRooks.count()*5 + self.blackQueens.count()*9
+        whiteMaterial = self.whitePawns.count()*100 + self.whiteBishops.count()*300 + self.whiteKnights.count()*300 + self.whiteRooks.count()*500 + self.whiteQueens.count()*900
+        blackMaterial = self.blackPawns.count()*100 + self.blackBishops.count()*300 + self.blackKnights.count()*300 + self.blackRooks.count()*500 + self.blackQueens.count()*900
         return whiteMaterial - blackMaterial
+    
+    def evalPositioning(self):
+        value = 0
+        for i in range(64):
+            if self.whitePawns[i]:
+                value += self.PAWN_POS_TABLE[i]
+            elif self.blackPawns[i]:
+                value -= self.PAWN_POS_TABLE[i^56]
+            elif self.whiteKnights[i]:
+                value += self.KNIGHT_POS_TABLE[i]
+            elif self.blackKnights[i]:
+                value -= self.KNIGHT_POS_TABLE[i^56]
+            elif self.whiteBishops[i]:
+                value += self.BISHOP_POS_TABLE[i]
+            elif self.blackBishops[i]:
+                value -= self.BISHOP_POS_TABLE[i^56]
+            elif self.whiteRooks[i]:
+                value += self.ROOK_POS_TABLE[i]
+            elif self.blackRooks[i]:
+                value -= self.ROOK_POS_TABLE[i^56]
+            elif self.whiteQueens[i]:
+                value += self.QUEEN_POS_TABLE[i]
+            elif self.blackQueens[i]:
+                value -= self.QUEEN_POS_TABLE[i^56]
+            elif self.whiteKing[i]:
+                value += self.KING_POS_TABLE[i]
+            elif self.blackKing[i]:
+                value -= self.KING_POS_TABLE[i^56]
+        return value
+            
     
     def heuristicEval(self):
         if self.gameOver():
-            return self.getResult()*1000
-        return self.evalMaterial()
+            return self.getResult()*10000
+        mat_eval = self.evalMaterial()
+        pos_eval = self.evalPositioning()
+        return mat_eval+pos_eval
     
     def generateTTKey(self):
         return self.getFENString()
@@ -1094,7 +1186,7 @@ class Board:
                 return transpositionEntry.value
 
         if depth == 0:
-            value = self.quiescenceEval(20, alpha, beta)
+            value = self.quiescenceEval(8, alpha, beta)
             newEntry = TTEntry(0, value, 1, self.age())
             self.transpositionTable[key] = newEntry
             return value
@@ -1174,16 +1266,16 @@ class Board:
                 value = max(value, self.quiescenceEval(depth-1, alpha, beta))
                 self.unmake(move)
                 if value >= beta:
-                    newEntry = TTEntry(depth, value, 2, self.age())
+                    newEntry = TTEntry(0, value, 2, self.age())
                     self.transpositionTable[key] = newEntry
                     return value
                 if value > alpha:
                     alpha = value
                     beatAlpha = True
             if beatAlpha:
-                newEntry = TTEntry(depth, value, 1, self.age())
+                newEntry = TTEntry(0, value, 1, self.age())
             else:
-                newEntry = TTEntry(depth, value, 3, self.age())
+                newEntry = TTEntry(0, value, 3, self.age())
             self.transpositionTable[key] = newEntry
             return value
         else:
@@ -1194,16 +1286,16 @@ class Board:
                 value = min(value, self.quiescenceEval(depth-1, alpha, beta))
                 self.unmake(move)
                 if value <= alpha:
-                    newEntry = TTEntry(depth, value, 3, self.age())
+                    newEntry = TTEntry(0, value, 3, self.age())
                     self.transpositionTable[key] = newEntry
                     return value
                 if value < beta:
                     beta = value
                     beatBeta = True
             if beatBeta:
-                newEntry = TTEntry(depth, value, 1, self.age())
+                newEntry = TTEntry(0, value, 1, self.age())
             else:
-                newEntry = TTEntry(depth, value, 2, self.age())
+                newEntry = TTEntry(0, value, 2, self.age())
             self.transpositionTable[key] = newEntry
             return value
     
@@ -1440,9 +1532,9 @@ def bitMaskString(bits):
     return result
 
 def main():
-    board = Board()
+    board = Board('2r1qk2/p5pQ/1p2pn2/6r1/3RP3/P6R/2p2K1P/2B5 w - - 2 32')
     print(board.getString())
-    print(board.playGame(2))
+    print(board.playGame(1))
 
 if __name__ == "__main__":
     main()
